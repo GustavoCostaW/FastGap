01ae50f3e444aaa420a821fac11ce7ec1b973a78
Renan Martins <renan@ead.cesumar.br>
2013-10-24 13:13:13 -0200

    Mudando o nome da funçao, deixando um sufixo (flickr e Intagram) a exemplo básico de como consumir a API do instagram

--- js/requestJson.js
+++ js/requestJson.js
@@ -1,5 +1,8 @@
-function requestJsonJS(){
 
+/**
+ * BUSCAR FOTOS NO FLICKR
+ */
+function requestJsonJSFlickr(){
 	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 	$.getJSON( flickerAPI, {
 		tags: "malteses",
@@ -13,5 +16,27 @@ function requestJsonJS(){
 				return false;
 			}
 		});
+	});
+}
+
+/**
+ * BUSCAR FOTOS NO INSTAGRAM
+ */
+function requestJsonJSInstagram(){
+
+	var instagramAPI = {}; 
+	
+	instagramAPI.Config = {
+		clientID: 'fd8538825477422db9da6dd26ee25a4f',
+		url: 'https://api.instagram.com',
+		tag: 'brasil'
+	};
+
+	url = instagramAPI.Config.url + "/v1/tags/" + instagramAPI.Config.tag + "/media/recent?callback=?&client_id=" + instagramAPI.Config.clientID;
+
+	$.getJSON(url, function( photo ){
+		$.each( photo.data, function( i, item ) {
+			$( "<img>" ).attr( "src", item.images.standard_resolution.url).appendTo( "#images-instagram" );
+		})
 	});
 }
